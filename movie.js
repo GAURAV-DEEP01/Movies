40a2efc28c1579dc89907468955b12b703bb0f96
Gaurav deep <118678150+GAURAV-DEEP01@users.noreply.github.com>
2023-06-09 19:46:13 +0530

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,43 +1,57 @@
 let serarchResult = document.getElementById('searchresult')
 let allCards = document.querySelector('.allcards')
 let cardContainer = document.querySelector('.card_container')
+let cardContainerTwo = document.querySelector('.card_container_two')
+let cardContainerSearch = document.querySelector('.card_container_search')
+let topRated = document.querySelector('.top_rated')
+let homeUpcomming = document.querySelector('.home_upcomming')
+let searchContainer = document.querySelector('.search_container')
+let searchText = document.querySelector('.search_text')
+
 const body = document.body
 let serarchbtn = document.getElementById('searchbtn')
-
-const url = 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming';
-
-const urlHome ='https://moviesdatabase.p.rapidapi.com/titles?startYear=2013&titleType=movie&list=top_rated_250';
+let selectables = document.querySelector('.selectables')
+const upcomingUrlHome = 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming';
+const topUrlHome = 'https://moviesdatabase.p.rapidapi.com/titles?startYear=2013&titleType=movie&list=top_rated_250';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '84e6135305mshf6dba2310292fb6p1b9b3ajsn3aa29744acc4',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '84e6135305mshf6dba2310292fb6p1b9b3ajsn3aa29744acc4',
+    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+  }
 };
-asyncCall()
-async function asyncCall() {
+
+homePageCards(topUrlHome)
+setTimeout(()=>{
+  homePageCards(upcomingUrlHome)
+},3000)
+async function homePageCards(url) {
   try {
-    const response = await fetch(urlHome, options);
+    const response = await fetch(url, options);
     const allresult = await response.json();
     console.log(allresult.results);
-    allresult.results.forEach(value=>{
-       console.log(value.id)
-        fetch(`https://moviesdatabase.p.rapidapi.com/titles/${value.id}/ratings`,options)
-        .then((ratingresponse)=>ratingresponse.json())
-        .then((ratingdata)=>{
-          console.log(ratingdata.results.averageRating)
-          creatCarde(value.primaryImage.url,value.originalTitleText.text,value.releaseYear.year,ratingdata.results.averageRating)
-        }) 
-  })
+    allresult.results.forEach(value => {
+      if (url == topUrlHome) {
+        fetch(`https://moviesdatabase.p.rapidapi.com/titles/${value.id}/ratings`, options)
+          .then((ratingresponse) => ratingresponse.json())
+          .then((ratingdata) => {
+            creatCard(value.primaryImage.url, value.originalTitleText.text, value.releaseYear.year, ratingdata.results.averageRating, cardContainer)
+          })
+      } else {
+        creatCard(value.primaryImage.url, value.originalTitleText.text, value.releaseYear.year, "not rated", cardContainerTwo)
+      }
+    })
   } catch (error) {
     console.error(error);
-  }}
-
+  }
+}
 serarchbtn.addEventListener('click', () => {
   clearAllcards()
+  topRated.style.display="none"
+  homeUpcomming.style.display="none"
   let searchtxt = serarchResult.value.trim()
-  let url = `http://www.omdbapi.com/?apikey=caf49d7e&s=${searchtxt}&plot=Short`;
-  fetch(url)
+  let searchUrl = `http://www.omdbapi.com/?apikey=caf49d7e&s=${searchtxt}&plot=Short`;
+  fetch(searchUrl)
     .then((res) => res.json())
     .then((data) => {
       data.Search.forEach(value => {
@@ -45,18 +59,19 @@ serarchbtn.addEventListener('click', () => {
         fetch(url2)
           .then((result) => result.json())
           .then((thedata) => {
-            creatCarde(value.Poster,value.Title,value.Year,thedata.Ratings[0].Value)
+            searchContainer.style.display="block"
+            searchText.innerText=`Search results for ${searchtxt}`
+            creatCard(value.Poster, value.Title, value.Year, thedata.Ratings[0].Value,cardContainerSearch)
             console.log(thedata)
           })
-          .catch(error=>console.console.log(error))
+          .catch(error => console.console.log(error))
       });
     })
     .catch((error) => console.log("error", error))
 })
-
-const creatCarde = (img,title,year,rate)=>{
+const creatCard = (img, title, year, rate, container) => {
   let theCard = document.createElement('div')
-  theCard.setAttribute('class','deletable')
+  theCard.setAttribute('class', 'selectables deletable')
   theCard.innerHTML = ` <div class="col h-100">
         <div class="card border-dark bg-transparent h-100">
           <img class="card-img-top" src="${img}" onerror="this.onerror=null;this.src='images/imageError.jpg';" alt="${title} img">
@@ -64,16 +79,28 @@ const creatCarde = (img,title,year,rate)=>{
             <h5 class="card-title">${title}</h5>
             <p class="card-text m-1"></p>
             <p class="badge bg-primary m-1 text-wrap">${rate}</p>
-            <p class="card-text m-1">year: ${year}</p>
+            <p class="card-text mt-1">year: ${year}</p>
           </div>
         </div>
       </div>`
-      cardContainer.append(theCard)
+  theCard.addEventListener('click', e => {
+    console.log(title)
+  }, { capture: true })
+  container.append(theCard)
 }
+// setInterval(() => {
+//    let selectables= document.querySelectorAll('.selectables')
+//    selectables.forEach(value=>{
+//      value.addEventListener('click',()=>{
+//       console.log('click')
+//      })
+//   })
+// }, 1000);
 
-const clearAllcards = ()=>{
-  const deletableCard= document.querySelectorAll('.deletable')
-  deletableCard.forEach(value=>{
+const clearAllcards = () => {
+  const deletableCard = document.querySelectorAll('.deletable')
+  deletableCard.forEach(value => {
     value.remove();
   })
 }
+
